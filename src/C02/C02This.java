7040d705c98a5421c4fa4a7e6a0181ab2640e2eb
wonmin <dds05197@naver.com>
2022-10-14 14:42:04 +0900

    JavaStudy

--- src/C02/C02This.java
+++ src/C02/C02This.java
@@ -1,6 +1,7 @@
 package C02;
 
 
+import java.util.Scanner;
 
 public class C02This {
     public static void main(String[] args) {
@@ -16,13 +17,27 @@ public class C02This {
         blength = str2.length();
         achar = str1.charAt(5);
         bchar = str2.charAt(10);
-        System.out.println("Str1에 저장 ="+str1 + "길이 ="+alength);
-        System.out.println("Str2에 저장 ="+str2 + "길이 ="+blength);
-        System.out.println("Str1의 5번째 문자 ="+achar);
-        System.out.println("Str1의 10번째 문자 ="+bchar);
-        System.out.println("Str1의 소문자로 변환 : "+str1.toUpperCase());
-        System.out.println("Str1의 소문자로 변환 : "+str1.toLowerCase());
-        System.out.println("Str2의 a를 A로 변환 ="+str2.replace('a','A'));
+        System.out.println("Str1에 저장 =" + str1 + "길이 =" + alength);
+        System.out.println("Str2에 저장 =" + str2 + "길이 =" + blength);
+        System.out.println("Str1의 5번째 문자 =" + achar);
+        System.out.println("Str1의 10번째 문자 =" + bchar);
+        System.out.println("Str1의 소문자로 변환 : " + str1.toUpperCase());
+        System.out.println("Str1의 소문자로 변환 : " + str1.toLowerCase());
+        System.out.println("Str2의 a를 A로 변환 =" + str2.replace('a', 'A'));
 
+        //trim()
+        //subString()
+        Scanner sc = new Scanner(System.in);
+        String tmp = sc.nextLine();
+
+        System.out.println(tmp);
+        System.out.println(tmp.trim());
+        //subString() : 문자열 자르기
+        System.out.println(tmp.substring(3));
+        System.out.println(tmp.substring(3 ,5));
+        System.out.println(tmp.indexOf("H"));
+        //lastIndexOf("문자열") : 문자열의 index 번호 확인
+        System.out.println(tmp.lastIndexOf("H"));
+        //contains("문자열") : 문자열 포함여부
     }
 }
